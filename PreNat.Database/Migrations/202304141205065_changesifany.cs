4a7ec689231990b7314d2cb30454f76c5081d9f9
CSC19F130 Student SMIU <CSC19F130@stu.smiu.edu.pk>
2023-04-16 13:08:11 +0500

    Nothin

---
+++ PreNat.Database/Migrations/202304141205065_changesifany.cs
@@ -0,0 +1,20 @@
+﻿namespace PreNat.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changesifany : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.LaboratoryTests", "VIH_L", c => c.String());
+            DropColumn("dbo.LaboratoryTests", "VIH");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.LaboratoryTests", "VIH", c => c.String());
+            DropColumn("dbo.LaboratoryTests", "VIH_L");
+        }
+    }
+}
